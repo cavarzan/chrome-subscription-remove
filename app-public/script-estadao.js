7066731e57cb201ae47d490b7d33f163e5e0e663
Deividi <deividi.cavarzan@gmail.com>
2014-12-12 23:58:52 -0200

    Update script-estadao.js

--- app-public/script-estadao.js
+++ app-public/script-estadao.js
@@ -1,4 +1,12 @@
 setInterval(function () {
+	var data = {
+		paywall_ligado : 0,
+		signwall_ligado : 0
+	};
+	var p = new PayWall(data);
+	p.initCount();
+	setCookie("OESP_PW_CONT", 0, 31);
+	
     $('.window').hide();
     $('#mask').hide();
     $('#pw-mask').hide();
@@ -16,26 +24,7 @@ setInterval(function () {
         document.body.detachEvent('touchmove', scrollTop, false);
         document.body.detachEvent('touchend', scrollTop, false);
     }
-
-} , 1000);
-setInterval(function () {
-    $('.window').hide();
-    $('#mask').hide();
-    $('#pw-mask').hide();
-    $('#pw-content').hide();
-
-    if(typeof oReload != 'undefined') oReload.start();
-    $('html').css('overflow','auto');
-
-    if(document.body.addEventListener){
-        document.body.removeEventListener('touchstart', scrollTop, false);
-        document.body.removeEventListener('touchmove', scrollTop, false);
-        document.body.removeEventListener('touchend', scrollTop, false);
-    }else{
-        document.body.detachEvent('touchstart', scrollTop, false);
-        document.body.detachEvent('touchmove', scrollTop, false);
-        document.body.detachEvent('touchend', scrollTop, false);
-    }
+	
+	
 
 } , 2000);
-
